02522e3c27b65dc4a27ff1d0c457bee2d94bebe5
Kondo Takeo <takeoverjp@gmail.com>
2019-01-06 18:06:26 +0900

    [2.2] Add gcd.

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,16 @@
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("gcd(48, 36) = {}", gcd(48, 36));
 }
