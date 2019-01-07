bb5e63b5f16083fc9b3e2b536b86a63c2dae44fa
Kondo Takeo <takeoverjp@gmail.com>
2019-01-07 16:55:33 +0900

    [2.6] Judge mandelbrot or not.

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,31 +1,21 @@
 extern crate num;
 use num::Complex;
 
-fn complex_square_add_loop(c: Complex<f64>) {
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex { re: 0., im: 0. };
-    loop {
+    for i in 0..limit {
         z = z * z + c;
-        println!("{:?}", z);
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
     }
-}
 
-#[allow(dead_code)]
-fn square_add_loop(c: f64) {
-    let mut x = 0.;
-    loop {
-        x = x * x + c;
-        println!("{}", x);
-    }
-}
-
-#[allow(dead_code)]
-fn square_loop(mut x: f64) {
-    loop {
-        x = x * x;
-        println!("{}", x);
-    }
+    None
 }
 
 fn main() {
-    complex_square_add_loop(Complex { re: 0.1, im: 0.1});
+    match escape_time(Complex {re: 3., im: 0.3}, 10000) {
+        Some(v) => println!("Not mandelbrot!"),
+        None => println!("Mandelbrot!")
+    }
 }
