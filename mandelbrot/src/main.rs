b446532a0b0ae43eec0e86d3a61f6c23ebbee726
Kondo Takeo <takeoverjp@gmail.com>
2019-01-07 17:18:45 +0900

    [2.6] Impl pixel_to_point.

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -51,6 +51,29 @@ fn test_parse_complex() {
     assert_eq!(parse_complex(",-0.0625"), None);
 }
 
+fn pixel_to_point(bounds: (usize, usize),
+                  pixel: (usize, usize),
+                  upper_left: Complex<f64>,
+                  lower_right: Complex<f64>)
+                  -> Complex<f64>
+{
+    let (width, height) = (lower_right.re - upper_left.re,
+                           upper_left.im - lower_right.im);
+
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 100), (25, 75),
+                              Complex { re: -1.0, im:  1.0 },
+                              Complex { re:  1.0, im: -1.0 }),
+               Complex { re: -0.5, im: -0.5 });
+}
+
 fn main() {
     match escape_time(Complex { re: 3., im: 0.3 }, 10000) {
         Some(v) => println!("Not mandelbrot! count = {}", v),
