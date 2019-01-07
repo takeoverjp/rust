797a5943695c5e161b216fb0f163c744a6e978ba
Kondo Takeo <takeoverjp@gmail.com>
2019-01-07 17:02:15 +0900

    [2.6] Impl parse_pair.

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,4 +1,6 @@
 extern crate num;
+
+use std::str::FromStr;
 use num::Complex;
 
 fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
@@ -13,9 +15,32 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     None
 }
 
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None
+            }
+        }
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10,", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
 fn main() {
     match escape_time(Complex {re: 3., im: 0.3}, 10000) {
-        Some(v) => println!("Not mandelbrot!"),
+        Some(v) => println!("Not mandelbrot! count = {}", v),
         None => println!("Mandelbrot!")
     }
 }
