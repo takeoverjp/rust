65279337466cd4b49aa723a5715bd970f8c04e75
Kondo Takeo <takeoverjp@gmail.com>
2019-01-07 18:12:29 +0900

    [2.6] Impl mandelbrot in paralell.

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,9 +1,15 @@
 extern crate num;
+extern crate image;
+extern crate crossbeam;
 
 use std::str::FromStr;
 use std::io::Write;
 use std::io::stderr;
 use num::Complex;
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
+
 
 fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex { re: 0., im: 0. };
@@ -95,12 +101,6 @@ fn render(pixels: &mut [u8],
     }
 }
 
-extern crate image;
-
-use  image::ColorType;
-use image::png::PNGEncoder;
-use std::fs::File;
-
 fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
                -> Result<(), std::io::Error>
 {
@@ -126,7 +126,23 @@ fn main() {
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render(&mut pixels, bounds, upper_left, lower_right);
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+                spawner.spawn(move || {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        });
+    }
 
     write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
