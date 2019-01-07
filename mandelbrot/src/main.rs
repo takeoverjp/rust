f6929a230b4295e7f1714a95a33cdf34edeaef16
Kondo Takeo <takeoverjp@gmail.com>
2019-01-07 17:30:15 +0900

    [2.6] Impl write_image.

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -93,6 +93,22 @@ fn render(pixels: &mut [u8],
     }
 }
 
+extern crate image;
+
+use  image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
+
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
+               -> Result<(), std::io::Error>
+{
+    let output = File::create(filename)?;
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+
+    Ok(())
+}
+
 fn main() {
     match escape_time(Complex { re: 3., im: 0.3 }, 10000) {
         Some(v) => println!("Not mandelbrot! count = {}", v),
